bb58e9341dcbd9ad14c740d440cf2031f8462275
David Kim <davidykim10@gmail.com>
2019-09-30 22:22:30 -0400

    Initial Commit

---
+++ client/main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Players, calculatePlayerPositions} from './../imports/api/players';
+import App from './../imports/ui/App';
+import {Tracker} from 'meteor/tracker';
+
+
+Meteor.startup(()=> {
+    Tracker.autorun(function() {
+        const players = Players.find({}, {
+            sort : {
+                score: -1,
+            }
+        }).fetch();
+        let positionedPlayers = calculatePlayerPositions(players);
+        let title = 'Score Keep';
+        
+        ReactDOM.render(<App title={title} players={positionedPlayers}/>, document.getElementById('app'));
+    }); 
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Score Keep Application
+
+A simple score keep web application that utilizes Meteor and React.
